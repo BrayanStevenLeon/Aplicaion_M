828ec23b25cf3914b66810629c18a921d2daad5c
Brayan leon <bragim1721@gmail.com>
2023-08-24 19:09:30 -0500

    Comentarios

--- Aplicacion/src/aplicacion/Aplicacion.java
+++ Aplicacion/src/aplicacion/Aplicacion.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class Aplicacion {
 
     public static void main(String[] args) {
-        Thread songThread = new Thread(() -> playSong("cancion.wav")); // Cambia "cancion.wav" por la ruta de tu archivo de audio
+        Thread songThread = new Thread(() -> playSong("cancion.wav")); 
         Thread gifThread = new Thread(Aplicacion::showGif);
 
         songThread.start();
@@ -23,7 +23,7 @@ public class Aplicacion {
             clip.open(audioStream);
             clip.start();
 
-            // Espera hasta que la canción termine de reproducirse
+           
             Thread.sleep(clip.getMicrosecondLength() / 1000);
         } catch (Exception e) {
             e.printStackTrace();
@@ -35,7 +35,7 @@ public class Aplicacion {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
 
-        ImageIcon gifIcon = new ImageIcon("animacion.gif"); // Cambia "animacion.gif" por la ruta de tu archivo GIF
+        ImageIcon gifIcon = new ImageIcon("animacion.gif"); 
         JLabel gifLabel = new JLabel(gifIcon);
 
         frame.getContentPane().add(gifLabel, BorderLayout.CENTER);
